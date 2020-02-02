0601e4b03678f55b3ce975cc83d072988a21bc0e
Isaac Pierce <contact@isaacdpierce.com>
2020-02-02 12:21:11 -0800

    Add background and obstacles. Refactored thrust and rotate functions.

--- Assets/Boost.cs
+++ Assets/Boost.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Boost : MonoBehaviour
 {
-
+  [SerializeField] float rcsThrust = 100f;
+  [SerializeField] float mainThrust = 100f;
   Rigidbody rigidBody;
   AudioSource audioSource;
   // Start is called before the first frame update
@@ -18,30 +19,57 @@ public class Boost : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-    ProcessInput();
+    Thrust();
+    Rotate();
   }
 
-  private void ProcessInput()
+  void OnCollisionEnter(Collision collision)
+  {
+    switch (collision.gameObject.tag)
+    {
+      case "Friendly":
+        print("You're OK!"); // TODO remove
+        break;
+      case "Power":
+        print("Power Up!"); // TODO remove
+        break;
+      default:
+        print("DEAD!"); // TODO remove
+        break;
+    }
+
+    print("BOOM!!!");
+  }
+  private void Thrust()
   {
     if (Input.GetKey(KeyCode.Space))
     {
-      rigidBody.AddRelativeForce(Vector3.up);
+      rigidBody.AddRelativeForce(Vector3.up * mainThrust);
       if (!audioSource.isPlaying)
       {
         audioSource.Play();
       }
-
     }
     else
     {
       audioSource.Stop();
     }
+  }
+
+  private void Rotate()
+  {
+    rigidBody.freezeRotation = true;
+    float rotationThisFrame = rcsThrust * Time.deltaTime;
 
     if (Input.GetKey(KeyCode.A))
     {
-      transform.Rotate(Vector3.forward);
+      transform.Rotate(Vector3.forward * rotationThisFrame);
     }
     else if (Input.GetKey(KeyCode.D))
-    { transform.Rotate(-Vector3.forward); }
+    {
+      transform.Rotate(-Vector3.forward * rotationThisFrame);
+    }
+    rigidBody.freezeRotation = false;
   }
+
 }
